61d6485322c69c7e7ecd5674266f5b0daeaa3a40
MaxMozdef <83218043+MaxMozdef@users.noreply.github.com>
2021-05-05 22:25:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Test
             for (; ; )
             {
                 q++;
-                Console.WriteLine("Hello Worlds111");
+                Console.WriteLine("Hello Worlds1112");
                 Console.WriteLine("or not?");
             }
                       
