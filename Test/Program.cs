9016a88e3056e37cddcf662f377be15541a01436
MaxMozdef <83218043+MaxMozdef@users.noreply.github.com>
2021-05-05 21:55:20 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int q = 0;
+            for (; ; )
+            {
+                q++;
+                Console.WriteLine("Hello Worlds!");
+            }
+                      
+        }        
+    }
+}
